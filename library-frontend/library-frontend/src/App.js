17a91b6105017f853e3826e282ca4a1e6de39faf
Raymond McCarthy <x00179223@mytudublin.ie>
2022-11-03 15:23:52 +0000

    implemented routing

--- library-frontend/library-frontend/src/App.js
+++ library-frontend/library-frontend/src/App.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import FooterComponent from './components/FooterComponent';
 import HeaderComponent from './components/HeaderComponent';
 import ListBooksComponent from './components/ListBooksComponent';
@@ -6,11 +8,18 @@ import ListBooksComponent from './components/ListBooksComponent';
 function App() {
   return (
     <div>
-      <HeaderComponent/>
-      <div className='container'>
-      <ListBooksComponent/>
-      </div>
-      <FooterComponent/>
+      <Router>
+        
+          <HeaderComponent/>
+            <div className="container">
+              <Routes> 
+                <Route exact path= "/" element = {<ListBooksComponent/>}/>
+                <Route exact path= "/books" element = {<ListBooksComponent/>}/>
+              </Routes>
+            </div>
+          <FooterComponent/>
+        
+      </Router>
     </div>
   );
 }
